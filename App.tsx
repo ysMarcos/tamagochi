6fe4321a99125d325bf4da5c8d0be1f90a258d98
ysMarcos <marguilherme02@gmail.com>
2023-09-08 15:49:30 -0300

    comit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Form from './src/screens/Form';
 import Home from './src/screens/Home';
-import { PopulacaoPage } from './src/screens/PopulacaoPage';
 import Login from './src/screens/Login';
 
 const Stack = createNativeStackNavigator();
@@ -14,8 +12,6 @@ function App(): JSX.Element {
     <Stack.Navigator>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Form" component={Form} />
-        <Stack.Screen name="Population" component={PopulacaoPage} />
     </Stack.Navigator>
   </NavigationContainer>
   )
